4fcf4ce1cad658a4cba063f8ccfd7743fa7a4091
JDiaz <jdiaz_007@outlook.com>
2023-10-13 00:05:12 -0700

    added notes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let greenCharacter = document.createElement('img')
+
+// * Tells the browser what to display
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
+
+// * Adds the image to the page. Without this line the img tag would be created,
+// *  but it wouldn't visible in the browser (only the tag in the console).
 document.body.append(greenCharacter)
